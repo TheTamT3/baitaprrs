24f8c7c6cd0e55f3f7d7dcdde3970b11672a1ca6
The Tam <phamthetam6@gmail.com>
2018-11-10 22:51:46 +0700

    first commit

---
+++ app/src/main/java/thetam/rss/dongNews.java
@@ -0,0 +1,38 @@
+package thetam.rss;
+
+public class dongNews {
+    private String title;
+    private String link;
+    private int image;
+
+    public dongNews(String title, String link, int image) {
+        this.title = title;
+        this.link = link;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
